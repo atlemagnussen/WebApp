c08251ec0106acbfcfeb340e021519b6953202aa
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2022-12-15 22:11:41 +0100

    Made landing page and centered links on middle of page

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -28,7 +28,7 @@ namespace WebApp
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=LandingPage}/{action=Index}/{id?}");
 
             app.Run();
         }
