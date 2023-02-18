320c25883e60cf4904d1dd396e3362b6f61aa54f
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2023-02-18 22:09:35 +0100

    Created ReportsController, preparing to submit and display sighting reports

---
+++ WebApp/Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿using WebApp.Data;
+using WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ReportsController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReportYourSighting()
+        {
+            return View();
+        }
+
+        public IActionResult SightingsArchive()
+        {
+            IEnumerable<Sighting> reports = _db.Sightings;
+
+            return View(reports);
+        }
+    }
+}
